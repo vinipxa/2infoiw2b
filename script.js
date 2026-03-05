1c402bcb36ebb68ac0ca0ef064aa5070487e4b75
Vinicius <portugavine@gmail.com>
2026-03-05 12:29:48 -0300

    novos bagulho

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var num = 0
 function sigma(){
-    document.getElementById(1).innerHTML = '<p>melhor personagem</p> <img src="/streetfightervini/sakura.jpg">'
+    num = num + 1;
+    console.log(num)
+    if (num % 2 != 0) {
+    document.getElementById(1).innerHTML = '<p>melhor personagem</p> <img id="sybau" src="/streetfightervini/sakura2.jpg">'    
+} else {
+    document.getElementById(1).innerHTML = '<p>melhor personagem</p> <img id="sybau" src="/streetfightervini/sakura.jpg">'      
+}
 }
