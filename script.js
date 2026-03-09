2ecb66586daee7224634afa3df5aa76f1fcd62d3
Vinicius <portugavine@gmail.com>
2026-03-09 08:13:10 -0300

    ta la ou não tá

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var num = 0
+const pai = document.getElementById('pai')
+const mae = document.getElementById('mae')
 function sigma(){
     num = num + 1;
-    console.log(num)
-    if (num % 2 != 0) {
-    document.getElementById(1).innerHTML = '<p>melhor personagem</p> <img id="sybau" src="/streetfightervini/sakura2.jpg">'    
+    console.log(num);
+    mae.style.display = 'block';
+    document.getElementById('irmao').innerHTML="DE NOVO!"
+    if (num % 2 != 0) { 
+        pai.innerHTML = "MELHOR PERSONAGEM"
+        mae.src = '/streetfightervini/sakura.jpg' 
 } else {
-    document.getElementById(1).innerHTML = '<p>melhor personagem</p> <img id="sybau" src="/streetfightervini/sakura.jpg">'      
+        pai.innerHTML = "SAKURA KASUGANO"
+        mae.src = '/streetfightervini/sakura2.jpg' 
 }
-}
+    if (num % 3 == 0){
+        mae.style.display = 'none';
+    }
+};
