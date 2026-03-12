552205c3ac69ed2e25ae727ef3dbc6927b37765d
Vinicius <portugavine@gmail.com>
2026-03-12 12:29:34 -0300

    outras paradas bizarras

--- script.js
+++ script.js
@@ -15,5 +15,35 @@ function sigma(){
 }
     if (num % 3 == 0){
         mae.style.display = 'none';
+        pai.innerHTML = "NÃOOO!!"
+        document.getElementById('irmao').innerHTML="Clique aqui para ver uma beldade"
     }
+    if (!document.getElementById("botaoJunior")){
+    const bu = document.createElement('button')
+    bu.id = "botaoJunior"
+    bu.innerText = "junior"
+    bu.onclick = figma
+    document.body.appendChild(bu)
+}
 };
+
+    function figma(){
+        var num1 = parseFloat(prompt("digita aqui:"));
+        var num2 = parseFloat(prompt("os dois né:"));
+        var resultado = num1 + num2;
+       document.getElementById('botaoJunior').innerText= (resultado)
+       window.print()
+       if (!document.getElementById("botaoJr")){
+    const but = document.createElement('button')
+    but.id = "botaojr"
+    but.innerText = "jr"
+    but.onclick = pigma
+    document.body.appendChild(but)
+}
+    }
+function pigma(){
+    var nomeAlunoEtec = "Vinição gostosão";
+    var idadeAluno = 16;
+    var alturaAluno = 1.80;
+    var estudandteAluno = true;
+}
